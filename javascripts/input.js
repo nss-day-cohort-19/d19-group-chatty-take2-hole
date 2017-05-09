e2e8c00ffbde7962adef848edb295b23040fb538
Tamela Lerma <tlerma83@gmail.com>
2017-05-09 12:39:47 -0500

    converted chatty to jquery

--- javascripts/input.js
+++ javascripts/input.js
@@ -1,37 +1,35 @@
 console.log('input has loaded');
 
 var Chatty= (function(altchatty){
-	var messageArr=[];
-	var counter=1;
-	altchatty.inputChange = function(elementid,message){
-			var outputDOM= document.getElementById(elementid);
-			outputDOM.innerHTML+= `<div id='newMessage--${counter}' class='tl-style-div'>
-				<span>${message}</span>
-                <br>
-				<button>Delete</button>
-				<button>Edit</button>
-				</div>`;
-        //this adds darkTheme toggle to new meassage buttons
-        var themeDiv = document.getElementById('newMessage--' + counter);
-            if (darkTheme.checked) {
-                var newMessageButt = themeDiv.getElementsByTagName("button");
-                newMessageButt[0].classList.toggle("tl-button");
-                newMessageButt[1].classList.toggle("tl-button");
-            }
+	let messageArr = [];
+	let counter = 1;
+	altchatty.inputChange = function(message){
 
-        //grabbed all the div tags which return an array, then said if the length of that array becomes greater than 20 remove the first index.
-            var messageCount = output.getElementsByTagName("div");
-            if (messageCount.length > 20) {
-                output.removeChild(messageCount[0]);
-            }
+        let messageCount = output.find("div");
+        let outputDOM = `<div id='newMessage--${counter}' class='tl-style-div'>
+            <span>${message}</span>
+            <br>
+            <button id='newDeleteBTN--${counter}'>Delete</button>
+            <button id='newEditBTN--${counter}'>Edit</button>
+            </div>`;
 
-			var newMessage= document.getElementById("newMessage--"+counter);
-			messageArr.push({id: `newMessage--${counter}`,span: message});
-			counter++;
-			console.log(messageArr);
-		}
+        output.append(outputDOM);
+
+        if (darkTheme.is(":checked")) {
+            $("#newDeleteBTN--" + counter).toggleClass("tl-button");
+            $("#newEditBTN--" + counter).toggleClass("tl-button");
+        }
+
+        if (messageCount.length > 20) {
+            $("#output div").first().remove();
+        }
+
+        messageArr.push({id: `newMessage--${counter}`,span: message});
+        counter++;
+    }
+
 	altchatty.privArr= function(argID){
-		for(var i=0; i< messageArr.length; i++){
+		for(let i = 0; i < messageArr.length; i++){
 			if(messageArr[i].id=== argID){
 				messageArr.splice(i,1);
 				break;
--- javascripts/load.js
+++ javascripts/load.js
@@ -2,21 +2,13 @@ console.log("testing load.js");
 
 var Chatty = (function(chat){
 
-	var messages = [];
-
 		chat.loadMessages= function (callBack){
-			var loader = new XMLHttpRequest();
-      		loader.open("GET", "messages.json");
-      		loader.send();
-      		loader.addEventListener("load", function (){
-      			messages = JSON.parse(this.responseText).message;
-      			callBack(messages);
-      		});
-      		loader.addEventListener("error", function(event){
-            console.log("Data did not load");
-        	});
+            $.getJSON("messages.json", function(result){
+                callBack(result.message);
+            });
 		}
 
 	return chat;
 
 })(Chatty || {});
+
